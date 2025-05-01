1a884e0a669a43a01e20709ae7ece987d8a3969f
Dahrkael <dark_wolf_warrior@hotmail.com>
2025-05-02 01:17:22 +0200

    add compile time config files

---
+++ config/dev.exs
@@ -0,0 +1,8 @@
+import Config
+
+config :extracker,
+  http_port: 6969,
+  debug: true
+
+config :logger,
+  level: :debug
---
+++ config/prod.exs
@@ -0,0 +1,8 @@
+import Config
+
+config :extracker,
+  http_port: 6969,
+  debug: false
+
+config :logger,
+  level: :info
