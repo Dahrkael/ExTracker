df240a690cf23739f8c8e0a8a42637ee9ea2c28c
Dahrkael <dark_wolf_warrior@hotmail.com>
2025-05-02 14:45:57 +0200

    * update config variables

--- config/dev.exs
+++ config/dev.exs
@@ -1,8 +1,16 @@
 import Config
 
 config :extracker,
+  debug: true,
+  http_enabled: true,
   http_port: 6969,
-  debug: true
+  https_enabled: false,
+  https_port: 7070,
+  udp_enabled: false,
+  udp_port: 6969,
+  scrape_enabled: true,
+  force_compact_peers: false,
+  return_external_ip: true
 
 config :logger,
   level: :debug
--- config/prod.exs
+++ config/prod.exs
@@ -1,8 +1,16 @@
 import Config
 
 config :extracker,
+  debug: false,
+  http_enabled: true,
   http_port: 6969,
-  debug: false
+  https_enabled: false,
+  https_port: 7070,
+  udp_enabled: true,
+  udp_port: 6969,
+  scrape_enabled: false,
+  force_compact_peers: true,
+  return_external_ip: false
 
 config :logger,
   level: :info
