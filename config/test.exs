d3984726a205fe2b960940a7219e35f0760784a9
Dahrkael <dark_wolf_warrior@hotmail.com>
2025-05-04 21:01:27 +0200

    + add test.exs config file

---
+++ config/test.exs
@@ -0,0 +1,21 @@
+import Config
+
+config :extracker,
+  debug: false,
+  http_enabled: true,
+  http_port: 6969,
+  https_enabled: false,
+  https_port: 7070,
+  udp_enabled: true,
+  udp_port: 6969,
+  udp_routers: -1,
+  udp_recbuf_size: 512_000,
+  udp_sndbuf_size: 512_000,
+  udp_buffer_size: 1_048_576,
+  connection_id_secret: 87178291199,
+  scrape_enabled: true,
+  force_compact_peers: false,
+  return_external_ip: true
+
+config :logger,
+  level: :debug
