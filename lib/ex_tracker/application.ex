81c42cd3f970228a78311766c7c32c398e266314
Dahrkael <dark_wolf_warrior@hotmail.com>
2025-05-27 01:16:35 +0200

    * missing tab

--- lib/ex_tracker/application.ex
+++ lib/ex_tracker/application.ex
@@ -2,7 +2,7 @@ defmodule ExTracker.Application do
   # See https://hexdocs.pm/elixir/Application.html
   # for more information on OTP Applications
   @moduledoc false
-require Logger
+  require Logger
 
   use Application
   alias ExTracker.Utils
