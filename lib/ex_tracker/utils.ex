b058a14de8f151e6ea99ad5a1e5c678656ec8055
Dahrkael <dark_wolf_warrior@hotmail.com>
2025-08-07 02:31:08 +0200

    + add util function

--- lib/ex_tracker/utils.ex
+++ lib/ex_tracker/utils.ex
@@ -1,5 +1,12 @@
 defmodule ExTracker.Utils do
 
+  def is_process_alive(name) do
+    case Process.whereis(name) do
+      nil -> false
+      pid -> Process.alive?(pid)
+    end
+  end
+
   def pad_to_8_bytes(bin) when byte_size(bin) < 8 do
     padding = :binary.copy(<<0>>, 8 - byte_size(bin))
     padding <> bin
